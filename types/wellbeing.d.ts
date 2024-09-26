71126e7c471b3f038bdf2c727b218fbb9c568379
samuCoronado <coronadolopezsamuel@gmail.com>
2024-09-26 00:06:39 -0600

    adds initial types

---
+++ types/wellbeing.d.ts
@@ -0,0 +1,30 @@
+import { Types } from 'mongoose';
+
+declare module 'wellbeing-types' {
+  export type Session = {
+    userId: Types.ObjectId | string;
+    sessionToken: Types.ObjectId | string;
+    expiresAt: Date;
+    createdAt: Date;
+  };
+
+  export type Token = {
+    userId: Types.ObjectId | string;
+    strategy: 'google' | 'github';
+    accessToken: string;
+    refreshToken: string;
+    createdAt: Date;
+  };
+
+  export type User = {
+    fullName: string;
+    username: string;
+    email: string;
+    emailVerified?: boolean;
+    createAt: Date;
+    updateAt?: Date;
+    profilePhoto?: string;
+    tokens?: Token[];
+    sessions?: Session[];
+  };
+}
